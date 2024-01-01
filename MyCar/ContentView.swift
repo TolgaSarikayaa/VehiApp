be98f890265acb0b3833e1bdf49196ed5bed7dcf
Tolga Sarikaya <113526329+TolgaSarikayaa@users.noreply.github.com>
2024-01-01 13:31:47 +0100

    first commit

---
+++ MyCar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCar
+//
+//  Created by Tolga Sarikaya on 01.01.24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+ 
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    ContentView()
+        
+}
---
+++ MyCar/MyCarApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyCarApp.swift
+//  MyCar
+//
+//  Created by Tolga Sarikaya on 01.01.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
