603839660daae8de4072f2a325e2e582833270a4
Tolga Sarikaya <113526329+TolgaSarikayaa@users.noreply.github.com>
2024-01-02 17:15:25 +0100

    RegisterView de bitti fakat HeaderView ile ilgili sorunlar var

--- MyCar/Views/LoginView.swift
+++ MyCar/Views/LoginView.swift
@@ -7,70 +7,104 @@
 
 import SwiftUI
 import SwiftData
+import Combine
+
+class KeyboardObserver: ObservableObject {
+    private var cancellable: AnyCancellable?
+    @Published var keyboardHeight: CGFloat = 0
+    
+    init() {
+        cancellable = NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .sink { notification in
+                guard let userInfo = notification.userInfo else { return }
+                guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+                self.keyboardHeight = keyboardFrame.height
+            }
+    }
+}
 
 struct LoginView: View {
-    
     @StateObject var viewModel = LoginViewViewModel()
- 
+    @StateObject var keyboardObserver = KeyboardObserver()
+    
+    
     var body: some View {
-            NavigationStack {
+        NavigationStack {
+            VStack {
+                // Header
+                HeaderView(title: "Welcome", subtitle: "Manage your car", background: UIImage(named: "background")!)
+                    .padding(.top, keyboardObserver.keyboardHeight > 0 ? -keyboardObserver.keyboardHeight : 0)
+                    .animation(.easeOut)
+                
+                Spacer()
+                
                 VStack {
-                    // Header
-                    HeaderView(title: "Welcome", subtitle: "Manage your car", background: UIImage(named: "background")!)
-                    
-                    Spacer()
-                    
-                    VStack {
-                        if !viewModel.erroMessage.isEmpty {
-                            Text(viewModel.erroMessage)
-                                .foregroundColor(.red)
-                        }
-                        
-                        HStack {
-                            Spacer()
-                            TextField("Email Address", text: $viewModel.email)
-                                .textFieldStyle(DefaultTextFieldStyle())
-                                .autocapitalization(.none)
-                                .padding(.horizontal, 20)
-                                .padding(.vertical, 10)
-                                .background(Color.white)
-                                .cornerRadius(8)
-                            Spacer()
-                        }
-                        
-                        HStack {
-                            Spacer()
-                            SecureField("Password", text: $viewModel.password)
-                                .textFieldStyle(DefaultTextFieldStyle())
-                                .padding(.horizontal, 20)
-                                .padding(.vertical, 10)
-                                .background(Color.white)
-                                .cornerRadius(8)
-                            Spacer()
-                        }
-                        
-                        MCButton(title: "Log In", background: .blue) {
-                            // Attempt log in
-                            viewModel.login()
-                        }
-                        .frame(height: 80)
-                        .padding()
+                    if !viewModel.erroMessage.isEmpty {
+                        Text(viewModel.erroMessage)
+                            .foregroundColor(.red)
                     }
-                    .offset(y: -50)
                     
-                    Spacer()
-                    
-                    // Create Account
-                    VStack {
-                        Text("New around here?")
-                        .foregroundColor(Color.white)
-                        NavigationLink("Create An Account", destination: RegisterView())
+                    HStack {
+                        Spacer()
+                        TextField("Email Address", text: $viewModel.email)
+                            .textFieldStyle(DefaultTextFieldStyle())
+                            .autocapitalization(.none)
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .foregroundColor(.black)
+                        Spacer()
                     }
-                    .padding(.bottom, 50)
+                    
+                    HStack {
+                        Spacer()
+                        SecureField("Password", text: $viewModel.password)
+                            .textFieldStyle(DefaultTextFieldStyle())
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .foregroundColor(.black)
+                        Spacer()
+                    }
+                    
+                    MCButton(title: "Log In", background: .blue) {
+                        // Attempt log in
+                        viewModel.login()
+                    }
+                    .frame(height: 80)
+                    .padding()
                 }
+                .offset(y: -50)
+                
+                Spacer()
+                
+                // Create Account
+                VStack {
+                    Text("New around here?")
+                        .foregroundColor(Color.white)
+                    NavigationLink("Create An Account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+             
             }
         }
+        .onAppear {
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardDidShowNotification, object: nil, queue: .main) { notification in
+                if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                    let keyboardHeight = keyboardSize.height
+                    self.keyboardObserver.keyboardHeight = keyboardHeight
+                }
+            }
+            
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
+                self.keyboardObserver.keyboardHeight = 0
+            }
+        }
+        .environmentObject(keyboardObserver)
     }
+}
 
 #Preview {
     LoginView()
--- MyCar/Views/RegisterView.swift
+++ MyCar/Views/RegisterView.swift
@@ -6,13 +6,72 @@
 //
 
 import SwiftUI
+import Combine
+
 
 struct RegisterView: View {
+   @StateObject var viewModel = RegisterViewViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Header
+            HeaderView(title: "Register", subtitle: "Start today", background: UIImage(named: "background")!)
+                
+            
+           
+            HStack {
+               Spacer()
+                TextField("Full Name", text: $viewModel.name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                    .autocapitalization(.none)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .foregroundColor(.black)
+                Spacer()
+            }
+            HStack {
+                Spacer()
+                TextField("Email Address", text: $viewModel.email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                    .autocapitalization(.none)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .foregroundColor(.black)
+                Spacer()
+                
+            }
+            
+            HStack {
+                Spacer()
+                SecureField("Password", text: $viewModel.password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .foregroundColor(.black)
+                Spacer()
+            }
+            
+            MCButton(title: "Creat Account", background: .green) {
+                // Attempt log in
+                viewModel.register()
+            }
+            .frame(height: 80)
+            .padding()
+        }
+        .offset(y: -50)
+    
     }
+      
 }
-
+    
 #Preview {
     RegisterView()
 }
