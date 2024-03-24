fdb800ee4850b655e8e78c8fbd933537abaf8b48
Tolga Sarikaya <113526329+TolgaSarikayaa@users.noreply.github.com>
2024-03-24 11:37:54 +0100

    Updated Json data and Localizable file

--- MyCar/Views/OnBoardingView/OnboardingData.swift
+++ MyCar/Views/OnBoardingView/OnboardingData.swift
@@ -15,11 +15,25 @@ struct OnboardingData: Hashable, Identifiable {
     
    
     static let list: [OnboardingData] = [
-        OnboardingData(id: 0, backgroundImage: "carRegister", primaryText: "Register Your Vehicle", secondaryText: "Easily manage and monitor your vehicles."),
-        OnboardingData(id: 1, backgroundImage: "ServiceTrack", primaryText: "Maintenance Management", secondaryText: "Ensure your vehicle remains in optimal condition by tracking maintenance records."),
-        OnboardingData(id: 2, backgroundImage: "FuelTrack", primaryText: "Fuel Expenditure Tracking", secondaryText: "Monitor and analyze your fuel spending to optimize your driving and save money.")
-    ]
-    
-}
+            OnboardingData(
+                id: 0,
+                backgroundImage: "carRegister",
+                primaryText: NSLocalizedString("Register Your Vehicle", comment: "Onboarding primary text for registering vehicle"),
+                secondaryText: NSLocalizedString("Easily manage and monitor your vehicles.", comment: "Onboarding secondary text for registering vehicle")
+            ),
+            OnboardingData(
+                id: 1,
+                backgroundImage: "ServiceTrack",
+                primaryText: NSLocalizedString("Maintenance Management", comment: "Onboarding primary text for maintenance management"),
+                secondaryText: NSLocalizedString("Ensure your vehicle remains in optimal condition by tracking maintenance records.", comment: "Onboarding secondary text for maintenance management")
+            ),
+            OnboardingData(
+                id: 2,
+                backgroundImage: "FuelTrack",
+                primaryText: NSLocalizedString("Fuel Expenditure Tracking", comment: "Onboarding primary text for fuel tracking"),
+                secondaryText: NSLocalizedString("Monitor and analyze your fuel spending to optimize your driving and save money.", comment: "Onboarding secondary text for fuel tracking")
+            )
+        ]
+    }
 
 
