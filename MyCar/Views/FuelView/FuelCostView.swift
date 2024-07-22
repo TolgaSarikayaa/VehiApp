089baf2ce646a4111f97264bed0d1b1d7c982fe8
Tolga Sarikaya <113526329+TolgaSarikayaa@users.noreply.github.com>
2024-07-22 22:33:48 +0300

    Added segment picker and fuel cost calculate function

--- MyCar/Views/FuelView/FuelCostView.swift
+++ MyCar/Views/FuelView/FuelCostView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct FuelCostView: View {
     @State private var fuelPrice = ""
+    @State private var fuelLiterPrice = ""
     @State private var overedTrack = ""
+    @State private var averagefuelConsumption = ""
     @Binding var show: Bool
     @State private var fuelConsumption: Double? = nil
+    @State private var totalCost: Double? = nil
+    @State private var selectedSegment = 0
     
 
     var body: some View {
@@ -25,26 +29,50 @@ struct FuelCostView: View {
                     .foregroundStyle(.gray, Color(.systemGray6))
             }
             
-            TextField("Fuel price", text: $fuelPrice)
-                .padding()
-                .keyboardType(.decimalPad)
-            TextField("Overed Track (Km)", text: $overedTrack)
-                .padding()
-                .keyboardType(.decimalPad)
+            Picker("Select Calculation", selection: $selectedSegment) {
+                Text("Fuel Consumption").tag(0)
+                Text("Total Cost").tag(1)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            if selectedSegment == 0 {
+                TextField("Fuel price", text: $fuelPrice)
+                    .padding()
+                    .keyboardType(.decimalPad)
+                TextField("Overed Track (Km)", text: $overedTrack)
+                    .padding()
+                    .keyboardType(.decimalPad)
+            } else {
+                TextField("total distance to travel", text: $overedTrack)
+                    .padding()
+                    .keyboardType(.decimalPad)
+                TextField("average fuel consumption", text: $averagefuelConsumption)
+                    .padding()
+                    .keyboardType(.decimalPad)
+                TextField("fuel liter price", text: $fuelLiterPrice)
+                    .padding()
+                    .keyboardType(.decimalPad)
+            }
+           
                 
         }
         
         VStack(alignment: .trailing) {
-            if let consumption = fuelConsumption {
+            if let consumption = fuelConsumption, selectedSegment == 0 {
                 Text("Fuel consumption: \(consumption, specifier: "%.2f") L/100km")
-                
+            } else if let cost = totalCost, selectedSegment == 1 {
+                Text("Fuel consumption: \(cost, specifier: "%.2f") $")
             }
         }
         
         HStack(spacing: 24){
             MCButton(title: "calculate", background: Color.blue) {
-                calculateFuelConsumption()
-                
+                if selectedSegment == 0 {
+                    calculateFuelConsumption()
+                } else {
+                    calculateTotalCost()
+                }
             }
         }.frame(height: 80)
          .padding(.horizontal)
@@ -55,14 +83,24 @@ struct FuelCostView: View {
             
             fuelConsumption = nil
             return
-           
         }
         
         let consumption = (fuelPriceDouble / overedTrackDouble) * 100
         fuelConsumption = consumption
     }
+    
+    private func calculateTotalCost() {
+        guard let totalDistance = Double(overedTrack), let averagefuelConsumption = Double(averagefuelConsumption), let fuelLiterPrice = Double(fuelLiterPrice), fuelLiterPrice > 0 else {
+            
+            totalCost = nil
+            return
+        }
+        
+        let calculateTotal = (totalDistance / 100) * averagefuelConsumption
+        let calculateFuel = calculateTotal * fuelLiterPrice
+        totalCost = calculateFuel
+    }
 }
-
 
 #Preview {
     FuelCostView(show: .constant(false))
